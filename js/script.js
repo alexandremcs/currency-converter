43663929bf1d43a93e1495b9653d6b2d1e9da93b
Alexandre <alexmcs@gmail.com>
2022-12-18 10:49:30 -0300

    Definindo opções selecionadas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 const dropList =  document.querySelectorAll(".drop-list select");
+const getBtn = document.querySelector("form button");
 
 for (let i = 0; i < dropList.length; i++) {
     for(currency_code in country_code){
-        let optionTag = `<option value="${currency_code}">${currency_code}</option>`;
+        
+        let selected;
+
+        if(i == 0){
+            selected = currency_code == "USD" ? "selected" : "";
+        }else if(i == 1){
+            selected = currency_code == "BRL" ? "selected" : "";
+        }
+
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
+
     }    
 }
