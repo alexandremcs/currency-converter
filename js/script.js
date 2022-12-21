59c297fcf3c6bc22ea889f9a63df167107d71024
Alexandre <alexmcs@gmail.com>
2022-12-21 17:03:04 -0300

    Implementando o carregamento das bandeiras

--- js/script.js
+++ js/script.js
@@ -16,9 +16,25 @@ for (let i = 0; i < dropList.length; i++) {
 
         let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
-
-    }    
+    }
+    dropList[i].addEventListener("change", e => {
+        loadFlag(e.target);
+    });   
 }
+
+function loadFlag(element){
+    for (code in country_code){
+        if (code == element.value){
+            let imgTag = element.parentElement.querySelector("img");
+            let countryFlag = country_code[code].toString().toLowerCase()
+            imgTag.src = `https://flagicons.lipis.dev/flags/4x3/${countryFlag}.svg`;
+        }
+    }
+}
+
+window.addEventListener("load", () => {
+    getExchangeRate();
+});
 
 getBtn.addEventListener("click", e => {
     e.preventDefault();
@@ -27,11 +43,20 @@ getBtn.addEventListener("click", e => {
 
 function getExchangeRate(){
     const amount = document.querySelector(".amount input");
+    exchangeRateTxt = document.querySelector(".exchange-rate");
+
     let amountValue = amount.value;
     if (amountValue == "" || amountValue == "0") {
         amount.value = "1"
         amountValue = '1'
     }
+    
+    exchangeRateTxt.innerText = `Aguardando conversão...`;
+
     let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`
-    fetch(url).then(response => console.log(response.json()));
+    fetch(url).then(response => response.json()).then(result => {
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExchangeRate = (amountValue * exchangeRate).toFixed(2);
+        exchangeRateTxt.innerText = `${amountValue} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
+    });
 }
